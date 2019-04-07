ede5a6778a7ccdf5f105582e9695c2377d03ed61
Sam Foucart <samfoucart25@gmail.com>
2019-04-06 23:43:49 -0400

    Added Bottom Navigation Bar

--- app/src/main/java/android/example/ohiouniversityspectrometerdatacollection/MainActivity.java
+++ app/src/main/java/android/example/ohiouniversityspectrometerdatacollection/MainActivity.java
@@ -1,13 +1,88 @@
 package android.example.ohiouniversityspectrometerdatacollection;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    // Debug
+    private static final String TAG = "MainActivity";
+
+    // Layout Views
+    private BottomNavigationView mBottomNav;
+    private ActionBar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolBar = getSupportActionBar();
+
+        // Setup Listener for bottom navigation bar
+        mBottomNav = findViewById(R.id.bottom_navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(navListener);
+        // Open the Spectrometer Settings Fragment first
+        mBottomNav.setSelectedItemId(R.id.nav_user_input);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new SpectrometerSettingsFragment()).commit();
+        // Set Toolbar title
+        mToolBar.setTitle(R.string.toolbar_spectrometer_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment;
+
+                    // Get the selected Fragment
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_files:
+                            mToolBar.setTitle(R.string.saved_files_title);
+                            selectedFragment = new FilesFragment();
+                            loadFragment(selectedFragment);
+                            return true;
+                        case R.id.nav_user_input:
+                            mToolBar.setTitle(R.string.toolbar_spectrometer_settings);
+                            selectedFragment = new SpectrometerSettingsFragment();
+                            loadFragment(selectedFragment);
+                            return true;
+                        case R.id.nav_graph:
+                            mToolBar.setTitle(R.string.graph_title);
+                            selectedFragment = new GraphFragment();
+                            loadFragment(selectedFragment);
+                            return true;
+                    }
+
+                    return false;
+                }
+            };
+
+    /**
+     * Replace and commit fragment
+     * @param fragment a non null fragment to be displayed
+     */
+    private void loadFragment(@NonNull Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        // Material Guidelines used to say not to add these to back stack, but Youtube adds to back
+        // stack, so they removed that, and I'm not sure which to pick.
+        // fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/android/example/ohiouniversityspectrometerdatacollection/SpectrometerSettingsFragment.java
@@ -0,0 +1,17 @@
+package android.example.ohiouniversityspectrometerdatacollection;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SpectrometerSettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_input, container, false);
+    }
+}
